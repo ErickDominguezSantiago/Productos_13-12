77b23860481cd02b29db8f6dc9bdaf887d715ce0
ErickDominguez <erick_dominguez_santiago@outlook.es>
2024-12-14 17:09:44 -0600

    se hace una correción en los parametros recibidos por la función createCards, así como una alternativa más para solucionar createCards

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const divProductos = document.getElementById("productos");
 const alertError =document.getElementById("alertError");
 const URLMain = "https://fakestoreapi.com/products";
 
-
+getData()
 
 
 function getData(){
@@ -19,20 +19,55 @@ function getData(){
 }
 
 
-function createCards(response){
-    response.forEach((response) => {
+function createCards(res){
+    res.forEach((Cards) => {
         divProductos.insertAdjacentHTML("beforeend",`
             <div class="col-sm">
             <div class="card" style=" align-items: center; width: 18rem; height:auto">
-                <img src="${response.image}" class="card-img-top" alt="..." style="width: 250px; height:300px">
+                <img src="${Cards.image}" class="card-img-top" alt="..." style="width: 250px; height:300px">
                 <div class="card-body">
-                    <h5 class="card-title">${response.title}</h5>
-                    <p class="card-text"><strong>${response.price}</strong></p>
-                    <p class="card-text">${response.description}</p>
-                    
+                    <h5 class="card-title">${Cards.title}</h5>
+                    <p class="card-text"><strong>${Cards.price}</strong></p>
+                    <p class="card-text">${Cards.description}</p>
                 </div></div>
             </div>`);
-        });
-}
+            });
+    }
 
-getData()
+    /////////////////////////////////////////////////////////////////////////////
+    // Otra forma para la mostar todso los objetos de un JSON desde un array
+
+
+// function CardHTML(responseTrue){
+//     divProductos.insertAdjacentHTML("beforeend",`
+//         <div class="col-sm">
+//         <div class="card" style=" align-items: center; width: 18rem; height:auto">
+//             <img src="${responseTrue.image}" class="card-img-top" alt="..." style="width: 250px; height:300px">
+//             <div class="card-body">
+//                 <h5 class="card-title">${responseTrue.title}</h5>
+//                 <p class="card-text"><strong>${responseTrue.price}</strong></p>
+//                 <p class="card-text">${responseTrue.description}</p>
+//             </div></div>
+//         </div>`);
+//     }
+    
+// function createCards(res){
+//     res.forEach(CardHTML);
+//     }
+
+
+
+// function createCards(response){
+//     response.forEach((response) => {
+//         divProductos.insertAdjacentHTML("beforeend",`
+//             <div class="col-sm">
+//             <div class="card" style=" align-items: center; width: 18rem; height:auto">
+//                 <img src="${response.image}" class="card-img-top" alt="..." style="width: 250px; height:300px">
+//                 <div class="card-body">
+//                     <h5 class="card-title">${response.title}</h5>
+//                     <p class="card-text"><strong>${response.price}</strong></p>
+//                     <p class="card-text">${response.description}</p>
+//                 </div></div>
+//             </div>`);
+//         });
+// }
